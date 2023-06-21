a04fc0ee621eaf755f11d13ec4d31f20ebeb0a16
cytescale <nikhil@cytescale.com>
2023-06-21 13:18:16 +0530

    commit all

---
+++ 1480. Running Sum of 1d Array.py
@@ -0,0 +1,9 @@
+nums = [1, 2, 3, 4, 5]
+res = []
+tr = 0
+
+for i in nums:
+    tr += i
+    res.append(tr)
+
+print(res)
--- 171. Excel Sheet Column Number.py
+++ 171. Excel Sheet Column Number.py
@@ -1,10 +1,10 @@
-
+# type: ignore
 
 columnTitle = "FXSHRXW"
 
 result = 0
 for i in range(len(columnTitle)):
-    value = ord(columnTitle[i]) - 64
+     value = ord(columnTitle[i]) - 64
     result += value * (26 ** (len(columnTitle) - i - 1))
 
 
